b061d071327140069510c489d9f153052930b251
j1ngzoue <workdrive0306@gmail.com>
2023-01-21 14:11:14 +0900

    chore: change to styles

---
+++ index.ts
@@ -0,0 +1,19 @@
+import fs from "fs";
+import postcss from "postcss";
+
+var css = fs.readFileSync("./dist/output.css");
+var root = postcss.parse(css); // `input.css`をパースしてASTを返す
+
+let outputStr = "";
+
+root.nodes.map((node) => {
+  if (node.type === "rule" && node.selector.startsWith(".")) {
+    for (const childNode of node.nodes) {
+      if (childNode.type === "decl") {
+        outputStr = `${outputStr}\n${childNode.prop}: ${childNode.value};`;
+      }
+    }
+  }
+});
+
+console.log("outputStr :>> ", outputStr);
